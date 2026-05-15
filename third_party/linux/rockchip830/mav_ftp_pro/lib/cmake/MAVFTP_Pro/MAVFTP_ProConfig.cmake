d8083bc2e3430c2c3fadcd979262f1921904e67b
hyh <xunxiaoh@gmail.com>
2026-05-15 16:51:14 +0800

    delete old third_party

--- third_party/linux/rockchip830/mav_ftp_pro/lib/cmake/MAVFTP_Pro/MAVFTP_ProConfig.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-if(NOT OFF)
-    find_dependency(Threads)
-    find_dependency(PahoMqttCpp)
-endif()
-
-get_filename_component(MAVSDK_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include("${MAVSDK_CMAKE_DIR}/MAVFTP_ProTargets.cmake")
